0c9c4db6f11ea00f33b8df8ee352890010a718a0
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-15 16:50:48 +0200

    Update page.tsx

--- src/app/(prestations)/devenir-auto-entrepreneur/declaration/page.tsx
+++ src/app/(prestations)/devenir-auto-entrepreneur/declaration/page.tsx
@@ -6,16 +6,17 @@ import NavBarLanding from "@/components/NavBarLanding";
 import Step from "@/components/Step";
 import StepManager from "@/components/StepManager";
 import FooterLanding from "@/components/footer/FooterLanding";
+import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
 
 const page = () => {
   return (
     <>
       <NavBarLanding />
-      <main className="min-h-[calc(100vh-140px)]">
+      <main className="min-h-[calc(100vh-100px)] md:bg-slate-50">
         <Banner value="Formulaire de déclaration d'activité auto-entrepreneur" />
         <MobilStepManager />
         <div className="w-screen">
-          <div className="max-w-6xl mx-auto md:mt-10 flex justify-between md:flex-row flex-col lg:px-8">
+          <div className="max-w-6xl mx-auto md:mt-10 bg-white rounded-xl flex justify-between md:flex-row flex-col p-5 lg:p-8 md:-translate-y-20">
             <div className="md:block hidden md:w-4/12 w-full">
               <Step />
             </div>
@@ -23,6 +24,20 @@ const page = () => {
             <div className="md:hidden block">
               <Step />
             </div>
+          </div>
+          <div className="max-w-6xl mx-auto md:-translate-y-14  p-5 lg:p-8">
+            <p className="text-slate-400 text-xs">
+              <ExclamationCircleIcon className="h-5 w-5 inline-block mr-2" />
+              Vos informations sont sécurisées conformément à la législation en
+              vigueur. Les renseignements collectés sur cette page sont utilisés
+              pour gérer votre commande, administrer vos paiements, et vous
+              contacter concernant votre démarche. Si vous avez souscrit à des
+              formules d&apos;abonnement, vos informations bancaires seront
+              également utilisées pour le prélèvement de vos paiements mensuels,
+              en fonction de votre date d&apos;adhésion. En accord avec le RGPD
+              et notre politique de confidentialité, vous avez le droit de
+              consulter, modifier et supprimer vos données personnelles.
+            </p>
           </div>
         </div>
       </main>
