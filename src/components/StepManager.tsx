e1954db32c71e99fd7e5523602efeaf0448be76b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-15 16:51:08 +0200

    Update StepManager.tsx

--- src/components/StepManager.tsx
+++ src/components/StepManager.tsx
@@ -146,8 +146,8 @@ const StepManager = () => {
             disabled={disabled}
             onClick={handleStep}
             className={`${
-              disabled ? "bg-cyan-900/70" : "bg-cyan-900"
-            }  text-white w-full py-2 rounded-md mt-10 hover:bg-cyan-900/70 transition duration-150 easeInOut`}
+              disabled ? "bg-green-700/70" : "bg-green-700"
+            }  text-white w-full py-2 rounded-md mt-10 hover:bg-green-700/70 transition duration-150 easeInOut`}
           >
             Continuer
           </button>
