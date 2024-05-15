d7f75e1849a9fae839b3fdae8b0ff988fc45d1bd
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-15 15:09:28 +0200

    Update Atouts.tsx

--- src/components/Atouts.tsx
+++ src/components/Atouts.tsx
@@ -1,26 +1,60 @@
 /* eslint-disable @next/next/no-img-element */
 const Atouts = () => {
   return (
-    <section className="py-24 overflow-hidden">
+    <section className="py-24 overflow-hidden bg-slate-700">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+        <div className="flex flex-col md:items-center">
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl md:text-center">
+            Pourquoi choisir <br />{" "}
+            <span className="">Info Auto-Entrepreneur</span> <br />
+            pour votre Auto-Entreprise?
+          </p>
+          <p className="my-6 text-lg leading-8 text-white md:text-center">
+            Avec Info Auto-Entrepreneur, lancer votre auto-entreprise en ligne
+            devient un jeu d&apos;enfant. Rejoignez le régime de la
+            micro-entreprise en tout juste 15 minutes et embarquez dans
+            l&apos;aventure de l&apos;auto-entrepreneuriat sans attendre.
+          </p>
+        </div>
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 items-center">
           <div className="lg:ml-auto lg:pl-4 lg:pt-4">
             <div className="lg:max-w-lg">
-              <p className="mt-2 text-3xl font-bold tracking-tight text-slate-700 sm:text-4xl">
-                Pourquoi choisir <br />{" "}
-                <span className="text-cyan-900">Info Auto-Entrepreneur</span>{" "}
-                <br />
-                pour votre Auto-Entreprise?
-              </p>
-              <p className="my-6 text-lg leading-8 text-slate-700">
-                Avec Info Auto-Entrepreneur, lancer votre auto-entreprise en
-                ligne devient un jeu d&apos;enfant. Rejoignez le régime de la
-                micro-entreprise en tout juste 15 minutes et embarquez dans
-                l&apos;aventure de l&apos;auto-entrepreneuriat sans attendre.
+              <ul className="my-6 text-lg leading-8 text-white list-disc list-inside">
+                <li>
+                  <span className="font-bold">Inscription rapide :</span> En
+                  seulement 15 minutes, vous pouvez compléter votre inscription
+                  et commencer votre activité.
+                </li>
+                <li>
+                  <span className="font-bold">Simplicité :</span> Nous vous
+                  guidons à chaque étape, rendant le processus facile et sans
+                  tracas.
+                </li>
+                <li>
+                  <span className="font-bold">Assistance dédiée :</span> Notre
+                  équipe est là pour répondre à toutes vos questions et vous
+                  aider en cas de besoin.
+                </li>
+                <li>
+                  <span className="font-bold">Accès aux ressources :</span>{" "}
+                  Profitez d&apos;un accès à une vaste gamme de ressources et de
+                  guides pour vous aider à réussir.
+                </li>
+                <li>
+                  <span className="font-bold">Communauté :</span> Rejoignez une
+                  communauté d&apos;auto-entrepreneurs partageant les mêmes
+                  idées et échangez des conseils et des expériences.
+                </li>
+              </ul>
+              <p className="my-6 text-lg leading-8 text-white">
+                En choisissant Info Auto-Entrepreneur, vous optez pour une
+                solution complète et efficace pour lancer et gérer votre
+                auto-entreprise. Nous sommes dédiés à votre réussite et nous
+                nous engageons à vous offrir le meilleur service possible.
               </p>
               <a
                 href="#form"
-                className={`bg-cyan-900 p-5 text-white w-full py-2 rounded-md mt-5 hover:bg-cyan-900/70 transition duration-150 easeInOut cursor-pointer`}
+                className={`p-5 text-slate-700 bg-white w-full py-2 rounded-md mt-5 transition duration-150 ease-in-out cursor-pointer`}
               >
                 Devenir Auto-Entrepreneur
               </a>
