2d5775f27cbbef42983149f6acdabf7f11a70cc9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-16 14:03:03 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,6 +12,8 @@ import { initializeCompte, setUserInfo } from "../redux/createUserSlice";
 const Hero = () => {
   const dispatch = useDispatch();
 
+  const [buttonLoader, setButtonLoader] = useState(false);
+
   const [formValues, setFormValues] = useState({
     nom: "",
     prenom: "",
@@ -59,6 +61,7 @@ const Hero = () => {
     }
 
     try {
+      setButtonLoader(true);
       // Utilisation de la fonction d'ajout
       await ajouterUtilisateurDansDossier(
         "brouillonInscription",
@@ -86,6 +89,8 @@ const Hero = () => {
       route.push("/devenir-auto-entrepreneur/declaration");
     } catch (error) {
       console.error("Erreur lors de l'ajout du brouillon utilisateur :", error);
+    } finally {
+      setButtonLoader(false);
     }
 
     // Dispatch l'action pour stocker les informations de l'utilisateur
@@ -118,30 +123,30 @@ const Hero = () => {
         id="form"
         className="w-full bg-[url('/background/papier.png')] md:bg-[url('/background/men.jpeg')] py-10 md:py-32 bg-no-repeat bg-cover md:bg-center bg-top relative background text-slate-700"
       >
-        <div className="px-6 lg:px-8 max-w-6xl mx-auto flex md:flex-row flex-col z-10 content">
-          <div className="md:w-1/2 w-full relative z-20">
+        <div className="z-10 flex flex-col max-w-6xl px-6 mx-auto lg:px-8 md:flex-row content">
+          <div className="relative z-20 w-full md:w-1/2">
             <img
               src="/logo.png"
               alt="hero"
-              className="md:w-48 w-32 h-auto absolute -top-3 -left-3 md:-top-5 md:-left-5 z-10 opacity-60"
+              className="absolute z-10 w-32 h-auto md:w-48 -top-3 -left-3 md:-top-5 md:-left-5 opacity-60"
             />
-            <h1 className="text-white font-bold sm:text-6xl text-4xl relative z-20">
+            <h1 className="relative z-20 text-4xl font-bold text-white sm:text-6xl">
               Créez votre <br /> auto-entreprise <br /> en quelques clics
             </h1>
-            <ul className="text-white mt-8 relative z-20">
-              <li className="mt-3 flex items-center text-normal">
+            <ul className="relative z-20 mt-8 text-white">
+              <li className="flex items-center mt-3 text-normal">
                 <div className="flex justify-center items-center h-[23px] w-[23px] mr-3 rounded-full bg-white/10">
                   <CheckCircleIcon className="min-w-[12px] h-7 w-7 text-cyan-700" />
                 </div>
                 Déclaration simplifiée au statut d&apos;auto-entrepreneur 2024
               </li>
-              <li className="mt-3 flex items-center text-normal">
+              <li className="flex items-center mt-3 text-normal">
                 <div className="flex justify-center items-center h-[23px] w-[23px] mr-3 rounded-full bg-white/10">
                   <CheckCircleIcon className="min-w-[12px] h-7 w-7 text-cyan-700" />
                 </div>
                 Déclaration simple et rapide
               </li>
-              <li className="mt-3 flex items-center text-normal">
+              <li className="flex items-center mt-3 text-normal">
                 <div className="flex justify-center items-center h-[23px] w-[23px] mr-3 rounded-full bg-white/10">
                   <CheckCircleIcon className="min-w-[12px] h-7 w-7 text-cyan-700" />
                 </div>
@@ -149,15 +154,24 @@ const Hero = () => {
               </li>
             </ul>
           </div>
-          <div className="md:w-1/2 w-full flex justify-center">
+          <div className="flex justify-center w-full md:w-1/2">
             <form
-              className="md:w-[80%] w-full py-8 bg-white md:bg-white/85 rounded-md px-5 md:px-8 flex-col items-center md:mt-0 mt-7"
+              className="md:w-[80%] w-full py-8 bg-white md:bg-white/85 rounded-md px-5 md:px-8 flex-col items-center md:mt-0 mt-7 relative"
               onSubmit={handleSubmit}
             >
-              <h2 className="text-center font-semibold w-9/12 mx-auto">
+              {buttonLoader && (
+                <div className="absolute top-0 right-0 flex items-center justify-center w-full h-full bg-white/50">
+                  <img
+                    src="/loader/loader.gif"
+                    alt="loader"
+                    className="w-10 h-10"
+                  />
+                </div>
+              )}
+              <h2 className="w-9/12 mx-auto font-semibold text-center">
                 FORMULAIRE DE DÉCLARATION EN LIGNE SIMPLIFIÉ
               </h2>
-              <div className=" flex justify-between mt-5">
+              <div className="flex justify-between mt-5 ">
                 <div className="w-full md:w-[45%]">
                   <label htmlFor="nom" className="text-slate-700">
                     Nom
@@ -167,7 +181,7 @@ const Hero = () => {
                     onChange={handleChange}
                     type="text"
                     placeholder="Votre nom"
-                    className="w-full border px-2 py-1 rounded-md border-slate-400 mt-2"
+                    className="w-full px-2 py-1 mt-2 border rounded-md border-slate-400"
                   />
                 </div>
                 <div className="w-[45%] md:block hidden">
@@ -179,11 +193,11 @@ const Hero = () => {
                     onChange={handleChange}
                     type="text"
                     placeholder="Votre prénom"
-                    className="w-full border px-2 py-1 rounded-md border-slate-400 mt-2"
+                    className="w-full px-2 py-1 mt-2 border rounded-md border-slate-400"
                   />
                 </div>
               </div>
-              <div className="w-full md:hidden block mt-3">
+              <div className="block w-full mt-3 md:hidden">
                 <label htmlFor="prenom" className="text-slate-700">
                   Prénom
                 </label>
@@ -192,7 +206,7 @@ const Hero = () => {
                   onChange={handleChange}
                   type="text"
                   placeholder="Votre prénom"
-                  className="w-full border px-2 py-1 rounded-md border-slate-400 mt-2"
+                  className="w-full px-2 py-1 mt-2 border rounded-md border-slate-400"
                 />
               </div>
               <div className="w-full mt-3">
@@ -204,10 +218,10 @@ const Hero = () => {
                   onChange={handleChange}
                   type="email"
                   placeholder="Votre email"
-                  className="w-full border px-2 py-1 rounded-md border-slate-400 mt-2"
+                  className="w-full px-2 py-1 mt-2 border rounded-md border-slate-400"
                 />
               </div>
-              <div className="w-full flex mt-3">
+              <div className="flex w-full mt-3">
                 <input
                   type="checkbox"
                   id="cgu"
@@ -215,7 +229,7 @@ const Hero = () => {
                   onChange={handleChange}
                   className="mr-2 cursor-pointer"
                 />
-                <label htmlFor="cgu" className="text-slate-700 ml-2 text-xs">
+                <label htmlFor="cgu" className="ml-2 text-xs text-slate-700">
                   J&apos;accepte les{" "}
                   <a href="#">conditions générales de vente</a> ainsi que la{" "}
                   <a href="#">politique de confidentialité</a>.
