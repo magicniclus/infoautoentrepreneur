adee38ae67c81fa92720472c81192794322d16c5
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-16 09:13:35 +0200

    Update Checkout.tsx

--- src/components/molecules/createUserStep/Checkout.tsx
+++ src/components/molecules/createUserStep/Checkout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { RootState } from "@/redux/store";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
@@ -116,87 +117,110 @@ const Checkout = () => {
   };
 
   return (
-    <div className="App">
-      <div className="w-full md:px-0 px-4">
-        <div className="mb-7 text-center bg-slate-200 text-slate-700 rounded-md py-4 px-2">
-          <div className="flex items-center text-xs w-[90%] mx-auto mb-7">
-            <CheckCircle className="h-4 w-4 text-cyan-900 mr-2" />
-            <h3 className="text-start">
-              Dossier enregistré{" "}
-              <span className="font-semibold">
-                le {dateActuelle} à {heureActuelle}
-              </span>
-            </h3>
-          </div>
-          <h2 className="">
-            {" "}
-            Paiement des frais à l’inscription au régime d’auto-entrepreneur :{" "}
-            <span className="font-bold">59,00 €</span>
-          </h2>
-          <div className="w-[90%] h-[1px] bg-slate-300 mx-auto mt-3"></div>
-          <div
-            className="mt-3 w-[90%] text-slate-400 mx-auto flex justify-between items-center cursor-pointer"
-            onClick={(e) => setOpen((e) => !e)}
-          >
-            <h3 className="flex items-center text-sm">
-              <span className="w-5 h-5 flex justify-center items-center rounded-full border border-slate-300 mr-2 items-center">
-                ?
-              </span>
-              Voir mes informations
-            </h3>
-            <ChevronDownIcon
-              className={`h-5 w-5 transition-all duration-150 easeInOut ${
-                open ? "transform rotate-180" : ""
-              }`}
-            />
-          </div>
-          {open && (
-            <div className="p-3 text-start w-[90%] mx-auto text-slate-400 text-sm">
-              <h3 className="">
-                Nom, prénom du demandant :{" "}
+    <>
+      <style jsx>{`
+        .background::before {
+          content: "";
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          height: 100%;
+          background: linear-gradient(
+            to right,
+            rgba(59, 70, 97, 0.53) 0.03%,
+            rgba(59, 70, 97, 0.2) 99.95%
+          );
+          z-index: 0;
+        }
+        .content {
+          position: relative;
+          z-index: 1;
+        }
+      `}</style>
+      <div className="App">
+        <div className="w-full md:px-0">
+          <div className="mb-7 text-center bg-blue-50 text-slate-700 rounded-md py-4 px-2">
+            <div className="flex items-center text-xs w-[90%] mx-auto mb-7">
+              <CheckCircle className="h-4 w-4 text-cyan-900 mr-2" />
+              <h3 className="text-start">
+                Dossier enregistré{" "}
                 <span className="font-semibold">
-                  {userNom.toUpperCase()} {userPrenom.toUpperCase()}
-                </span>
-              </h3>
-              <h3 className="mt-1.5">
-                Activité principale :{" "}
-                <span className="font-semibold">{userActivite}</span>
-              </h3>
-              <h3 className="mt-1.5">
-                Date de début d&apos;activité :{" "}
-                <span className="font-semibold">
-                  {formatDate(userDebutActivite)}
+                  le {dateActuelle} à {heureActuelle}
                 </span>
               </h3>
             </div>
+            <h2 className="">
+              {" "}
+              Paiement des frais à l’inscription au régime d’auto-entrepreneur :{" "}
+              <span className="font-bold">59,00 €</span>
+            </h2>
+            <div className="w-[90%] h-[1px] bg-slate-300 mx-auto mt-3"></div>
+            <div
+              className="mt-3 w-[90%] text-slate-400 mx-auto flex justify-between items-center cursor-pointer"
+              onClick={(e) => setOpen((e) => !e)}
+            >
+              <h3 className="flex items-center text-sm">
+                <span className="w-5 h-5 flex justify-center items-center rounded-full border border-slate-300 mr-2 items-center">
+                  ?
+                </span>
+                Voir mes informations
+              </h3>
+              <ChevronDownIcon
+                className={`h-5 w-5 transition-all duration-150 easeInOut ${
+                  open ? "transform rotate-180" : ""
+                }`}
+              />
+            </div>
+            {open && (
+              <div className="p-3 text-start w-[90%] mx-auto text-slate-400 text-sm">
+                <h3 className="">
+                  Nom, prénom du demandant :{" "}
+                  <span className="font-semibold">
+                    {userNom.toUpperCase()} {userPrenom.toUpperCase()}
+                  </span>
+                </h3>
+                <h3 className="mt-1.5">
+                  Activité principale :{" "}
+                  <span className="font-semibold">{userActivite}</span>
+                </h3>
+                <h3 className="mt-1.5">
+                  Date de début d&apos;activité :{" "}
+                  <span className="font-semibold">
+                    {formatDate(userDebutActivite)}
+                  </span>
+                </h3>
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="flex justify-between mb-7 md:px-0 md:mt-0">
+          <div className="flex items-center">
+            <LockClosedIcon className="h-5 w-5 text-yellow-500" />
+            <p className="text-slate-700 ml-2">Paiement sécurisé SSL</p>
+          </div>
+          <Image
+            src="/icons/payment.svg"
+            width={100}
+            height={20}
+            alt="Carte bancaire"
+            className="md:block hidden"
+          />
+        </div>
+        <div className="">
+          {clientSecret && (
+            <Elements
+              options={{ ...options, locale: "fr-FR" }}
+              stripe={stripePromise}
+            >
+              <CheckoutForm />
+            </Elements>
           )}
         </div>
       </div>
-
-      <div className="flex justify-between mb-7 md:px-0 px-4 md:mt-0">
-        <div className="flex items-center">
-          <LockClosedIcon className="h-5 w-5 text-yellow-500" />
-          <p className="text-slate-700 ml-2">Paiement sécurisé SSL</p>
-        </div>
-        <Image
-          src="/icons/payment.svg"
-          width={100}
-          height={20}
-          alt="Carte bancaire"
-          className="md:block hidden"
-        />
-      </div>
-      <div className="px-4">
-        {clientSecret && (
-          <Elements
-            options={{ ...options, locale: "fr-FR" }}
-            stripe={stripePromise}
-          >
-            <CheckoutForm />
-          </Elements>
-        )}
-      </div>
-    </div>
+    </>
   );
 };
 
