02b5ca19ed0bb1e2a73e89ea0f31a00d19045bd5
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-16 20:36:58 +0200

    Update CoordonneeCessation.tsx

--- src/components/molecules/createUserStep/CoordonneeCessation.tsx
+++ src/components/molecules/createUserStep/CoordonneeCessation.tsx
@@ -7,7 +7,7 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateField } from "@mui/x-date-pickers/DateField";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../redux/store";
@@ -79,28 +79,31 @@ const CoordonneeCessation = ({ data }: { data: any }) => {
     dispatch(setCessationDetails(updatedFormValues)); // Assurez-vous également de mettre à jour le store Redux
   };
 
-  const handleDateChange = (newValue: string) => {
-    const today = dayjs();
-    const birthdate = dayjs(newValue);
-    const age = today.diff(birthdate, "year");
+  const handleDateChange = (newValue: Dayjs | null) => {
+    if (newValue) {
+      const today = dayjs();
+      const birthdate = newValue;
+      const age = today.diff(birthdate, "year");
 
-    if (age < 18) {
-      setFormErrors((prevErrors) => ({
-        ...prevErrors,
-        dateDeNaissance: "Vous devez avoir au moins 18 ans.",
-      }));
-      // Dispatch avec dateDeNaissance vide si l'utilisateur a moins de 18 ans
-      const updatedFormValues = { ...formValues, dateDeNaissance: "" };
-      setFormValues(updatedFormValues);
-      dispatch(setCessationDetails(updatedFormValues));
-    } else {
-      setFormErrors((prevErrors) => ({
-        ...prevErrors,
-        dateDeNaissance: "",
-      }));
-      const updatedFormValues = { ...formValues, dateDeNaissance: newValue };
-      setFormValues(updatedFormValues);
-      dispatch(setCessationDetails(updatedFormValues));
+      if (age < 18) {
+        setFormErrors((prevErrors) => ({
+          ...prevErrors,
+          dateDeNaissance: "Vous devez avoir au moins 18 ans.",
+        }));
+        setFormValues((prevValues) => ({
+          ...prevValues,
+          dateDeNaissance: "",
+        }));
+      } else {
+        setFormErrors((prevErrors) => ({
+          ...prevErrors,
+          dateDeNaissance: "",
+        }));
+        setFormValues((prevValues) => ({
+          ...prevValues,
+          dateDeNaissance: newValue.format("YYYY-MM-DD"),
+        }));
+      }
     }
   };
 
@@ -269,9 +272,9 @@ const CoordonneeCessation = ({ data }: { data: any }) => {
           <CalendarIcon className="absolute top-3.5 right-3 text-slate-500 h-6 w-6 " />
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DemoContainer components={["DateField", "DateField", "DateField"]}>
-              <DateField // Convertissez la chaîne en objet dayjs ici
-                onChange={(newValue: string | null) =>
-                  handleDateChange(newValue as string)
+              <DateField
+                onChange={(newValue: Dayjs | null) =>
+                  handleDateChange(newValue)
                 }
                 format="DD/MM/YYYY"
                 sx={{
