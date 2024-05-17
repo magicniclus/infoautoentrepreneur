e99df2592eb27dec73e1ca569278b39d32b33f31
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-17 18:52:40 +0200

    Update CheckoutCessation.tsx

--- src/components/molecules/createUserStep/CheckoutCessation.tsx
+++ src/components/molecules/createUserStep/CheckoutCessation.tsx
@@ -100,7 +100,7 @@ const CheckoutCessation = () => {
           <h2 className="">
             {" "}
             Paiement des frais de cessation d&apos;activité :{" "}
-            <span className="font-bold">49,00 €</span>
+            <span className="font-bold">69,00€</span>
           </h2>
           <div className="w-[90%] h-[1px] bg-slate-300 mx-auto mt-3"></div>
           <div
